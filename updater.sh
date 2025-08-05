94b3e7155bc9093d0f07ba88e4d66972e531e026
Sean Rhodes <sean@starlabs.systems>
2025-08-05 21:05:06 +0100

    Update roms dir

--- updater.sh
+++ updater.sh
@@ -23,12 +23,12 @@ pushd "$WORKING_DIR" >/dev/null
 
 # Touchscreen
 function update_touchscreen() {
-	wget -q "$REPO/binaries/gdixupdate"
-	chmod +x gdixupdate
-	wget -q "$REPO/StarLite/MkV/touchscreen/GT7387P_00.A1.11.25(373E).bin"
-
 	for SYS in /sys/class/hidraw/hidraw*; do
 		if grep -qFx "hid:b0018g0004v000027C6p00000111" "$SYS/device/modalias"; then
+			wget -q "$REPO/binaries/gdixupdate"
+			chmod +x gdixupdate
+			wget -q "$REPO/StarLite/MkV/touchscreen/GT7387P_00.A1.11.25(373E).bin"
+
 			if sudo ./gdixupdate -d "/dev/$(basename "$SYS")" -s 7387 -f -i "$WORKING_DIR/GT7387P_00.A1.11.25(373E).bin"; then
 				echo "Touchscreen update complete."
 			else
@@ -40,9 +40,6 @@ function update_touchscreen() {
 
 # Keyboard
 function update_keyboard() {
-	wget -q "$REPO/binaries/kb-usb-flasher"
-	chmod +x kb-usb-flasher
-
 	current_version=$(lsusb -d 1018:1006 -v 2>/dev/null | awk '/bcdDevice/ { print $2; exit }')
 
 	# Odd numbers are US, even are UK
@@ -50,6 +47,9 @@ function update_keyboard() {
 		if [[ "$current_version" == "1.05" ]] || [[ "$current_version" == "1.06" ]]; then
 			echo "Keyboard is already up-to-date"
 		else
+			wget -q "$REPO/binaries/kb-usb-flasher"
+			chmod +x kb-usb-flasher
+
 			if [[ "$current_version" == "1.03" ]]; then
 				wget -q "$REPO/StarLite/MkV/keyboard/1.05.bin" -O kbfw.bin
 			elif [[ "$current_version" == "1.04" ]]; then
@@ -64,8 +64,6 @@ function update_keyboard() {
 				else
 					echo "Keyboard update failed!"
 				fi
-			else
-				echo "No keyboard detected"
 			fi
 		fi
 	fi
@@ -73,45 +71,27 @@ function update_keyboard() {
 
 # Coreboot
 function update_coreboot() {
-	echo "${YELLOW}Make sure flashrom 1.3.0 or newer is installed.${RESET}"
+	current_version=$(cat /sys/class/dmi/id/bios_version)
+	if [[ "$current_version" != "25.07" ]]; then
+		echo "${YELLOW}Make sure flashrom 1.3.0 or newer is installed.${RESET}"
 
-	wget "$REPO/binaries/reset-cmos"
-	chmod +x reset-cmos
-	wget "$REPO/roms/$SKU.bios"
-	read -p "Once updated, the system will automatically shutdown. Please [Enter] to continue"
+		wget "$REPO/binaries/reset-cmos"
+		chmod +x reset-cmos
+		wget "$REPO/roms/$SKU.bios"
+		read -p "Once updated, the system will automatically shutdown. Please [Enter] to continue"
 
-	[ "$SKU" = "B6-A" ] && flags="" || flags="--fmap -n -N -i COREBOOT -i EC"
-	if sudo flashrom -p internal -w "$SKU.bios" $flags; then
-		echo "coreboot update complete. System will now shutdown."
-		sudo ./reset-cmos
-		sudo shutdown now
-	else
-		echo "coreboot update failed."
+		[ "$SKU" = "B6-A" ] && flags="" || flags="--fmap -n -N -i COREBOOT -i EC"
+		if sudo flashrom -p internal -w "$SKU.bios" $flags; then
+			echo "coreboot update complete. System will now shutdown."
+			sudo ./reset-cmos
+			sudo shutdown now
+		else
+			echo "coreboot update failed."
+		fi
 	fi
 }
 
-while true; do
-	task=""
-	echo
-	echo "${YELLOW}===== Manual Firmware Update =====${RESET}"
-	PS3="${GREEN}Enter choice (0 to exit): ${RESET}"
-	if [[ "$SKU" == "I5" || "$SKU" == "I5-SB" ]]; then
-		options=(touchscreen keyboard coreboot)
-	else
-		options=(coreboot)
-	fi
-	select opt in "${options[@]}"; do
-		if [[ -z $opt ]]; then
-			echo "${YELLOW}Invalid selection.${RESET}"
-			continue
-		fi
-		task="$opt"
-		break
-	done
-
-	case "$task" in
-		touchscreen) update_touchscreen ;;
-		keyboard)    update_keyboard    ;;
-		coreboot)    update_coreboot    ;;
-	esac
-done
+update_touchscreen
+update_keyboard
+update_coreboot
+echo "All firmware up to date"
